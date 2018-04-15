56ec2b8f4721f6c7a87ef8c50b07aad7a0da7eec
znlove <2639323039@qq.com>
2018-04-15 20:54:24 +0800

    在main方法添加method方法

--- src/main/java/com/tqh/demo/DemoApplication.java
+++ src/main/java/com/tqh/demo/DemoApplication.java
@@ -6,7 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	/**
+	 * 新建方法
+	 */
+	public void method(){
+
 	}
 }
